9de1e7dad79c68eb73b0ba88cc644fbc94bfec85
MixFon <mixfon@rambler.ru>
2021-08-23 14:18:14 +0300

    add XML parsing

--- taskmaster/taskmaster/main.swift
+++ taskmaster/taskmaster/main.swift
@@ -7,5 +7,44 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Welcom to Taskmaster!")
 
+
+//func readFile(fileName: String) throws -> String {
+//		let manager = FileManager.default
+//		let currentDirURL = URL(fileURLWithPath: manager.currentDirectoryPath)
+//		let fileURL = currentDirURL.appendingPathComponent(fileName)
+//		return try String(contentsOf: fileURL)
+//}
+//
+//let str = try? readFile(fileName: "server_config.xml")
+//if str != nil {
+//	print(str!)
+//} else {
+//	print("Xui")
+//}
+
+//let parser = try? XMLDocument(xmlString: str!, options: .documentTidyXML)
+//
+//let manager = FileManager.default
+//let currentDirURL = URL(fileURLWithPath: manager.currentDirectoryPath)
+//let fileURL = currentDirURL.appendingPathComponent("server_config.xml")
+//let xmlDoc = try? XMLDocument(contentsOf: fileURL, options: .documentTidyXML)
+
+//let parser = XMLParser(contentsOf: fileURL)
+//print(xmlDoc?.rootElement()?.xmlString(options: .documentTidyXML) ?? "Xui2")
+//let parser = Process(contentsOf: fileURL)
+//let content = try String(contentsOf: fileURL)
+//let parser = Process(data: Data(content.utf8))
+//if parser.parse() {
+//	print(parser.attributeKeys)
+//} else {
+//	print(parser.parserError ?? "Xui?")
+//}
+//
+//guard let data = try? Data(contentsOf: fileURL) else { exit(-1) }
+//print(data.last!)
+//let parser = Process(data: data)
+//parser.delegate = self as? XMLParserDelegate
+//parser.parse()
+let process = Process()
